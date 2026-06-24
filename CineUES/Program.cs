6d7e0f442a54e2f0d96129f24633b09a029cce2d
Nameless <89994985+Cesar62@users.noreply.github.com>
2026-06-23 19:19:18 -0600

    Cesar (#10)
    * pequeñas correcciones de texto
    
    admeas le agregue que se vea en el ticket cuando hay descuentos
    
    * Pequeño else para agregar un espacio
    
    * correcion de salto de linea

--- CineUES/Program.cs
+++ CineUES/Program.cs
@@ -439,8 +439,12 @@ class CineUES
         {
             Console.WriteLine(" Descuento del 30%");
         }
+        else
+        {
+            Console.WriteLine("");
+        }
         Console.Write($"  Día       : {dias[dia]}");
-        if(dia == 3){ Console.WriteLine(" !!!Descuento del 20%!!!");} //mensaje si hay descuento por dia
+        if(dia == 3){ Console.WriteLine(" !!!Descuento del 20%!!!");}else{Console.WriteLine("");} //mensaje si hay descuento por dia
         Console.WriteLine("--------------------------------");
         Console.WriteLine($"  Subtotal  : ${precioSinIVA:F2}");
         Console.WriteLine($"  IVA (13%) : ${montoIVA:F2}");
